548fe861bd3d845be37919364c2d063fe11c6660
Landflier <vasil.r.yordanov@gmail.com>
2023-06-05 23:46:43 +0200

    awesome
    FIX: fixed autorun.sh script

--- awesome/autorun.sh
+++ awesome/autorun.sh
@@ -2,12 +2,12 @@
 run () {
 	if ! pgrep -f "$1" ;
 	then
-		"$@"&
+		eval "$@"&
 	fi
 }
 
 # window compositor picom (for terminal bg transparency)
-run "picom -f "
-
+run "picom -f"
+run "feh --no-fehbg --bg-scale '/home/darkness/.wallpapers/lighthouse.jpg'"
 #
-run "keychain --quiet "
+# run "keychain --quiet "
