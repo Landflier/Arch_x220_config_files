25a2c5e2f32725a6af4417fc85e63aa530c9d29f
Landflier <vasil.r.yordanov@gmail.com>
2023-06-12 22:41:33 +0200

    awesome-wm
    ADD: comments in the autorun.sh

--- awesome/autorun.sh
+++ awesome/autorun.sh
@@ -8,7 +8,11 @@ run () {
 
 # window compositor picom (for terminal bg transparency)
 run "picom -f"
+# feh for background image
 run "feh --no-fehbg --bg-scale '/home/darkness/.wallpapers/lighthouse.jpg'"
+# keyechain for ssh keys (used in Github)
 run "keychain --quiet --eval --noask Photoemission-git-doc github_Landflier id_ed25519"
+# Mathpix, if set as alias in .bashrc
+# run "mpix"
 #
 # run "keychain --quiet "
